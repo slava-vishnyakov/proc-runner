16e892e84220cb9d63034484b0df525b75821dc0
Slava Vishnyakov <bomboze@gmail.com>
2015-07-11 10:50:48 +0200

    Initial

---
+++ src/LocalRunner.php
@@ -0,0 +1,134 @@
+<?php
+
+require __DIR__ . '/RunnerInterface.php';
+
+class Runner implements RunnerInterface
+{
+    const PROCESS_WENT_AWAY = "Process went away, see readStderrLineIfAny() for any hints";
+
+    private $returnCode;
+
+    public function __construct($command, $commandArguments = null, $workingDirectory = null, $env = [], $procOpenOptions = [])
+    {
+        $descriptor = [["pipe", "r"], ["pipe", "w"], ["pipe", "w"]];
+
+        $pipes = [];
+
+        $escapedCommands = [$command];
+        if (is_array($commandArguments)) {
+            foreach ($commandArguments as $arg) {
+                array_push($escapedCommands, escapeshellarg($arg));
+            }
+        }
+
+        $cmd = join(' ', $escapedCommands);
+        $this->process = proc_open($cmd, $descriptor, $pipes, $workingDirectory, $env, $procOpenOptions);
+
+        $this->stdin = $pipes[0];
+        $this->stdout = $pipes[1];
+        $this->stderr = $pipes[2];
+
+        stream_set_blocking($this->stdin, 0);
+        stream_set_blocking($this->stdout, 0);
+        stream_set_blocking($this->stderr, 0);
+
+        if (!$this->isAlive()) {
+            throw new RuntimeException("Process cannot be started");
+        }
+    }
+
+    public function isAlive()
+    {
+        $status = proc_get_status($this->process);
+        return $status['running'] === true;
+    }
+
+    public function send($string)
+    {
+        $bytesSent = fwrite($this->stdin, $string);
+        if ($bytesSent == 0 && strlen($string) > 0) {
+            throw new RuntimeException(self::PROCESS_WENT_AWAY);
+        }
+    }
+
+    protected function _readLine($pipe, $timeout = null)
+    {
+        $start = microtime(true);
+        while (true) {
+            if (!$this->isAlive()) {
+                throw new RuntimeException(self::PROCESS_WENT_AWAY);
+            }
+            $string = fgets($pipe);
+            if ($string != '') {
+                return $string;
+            }
+
+            $null = [];
+            $read = [$pipe];
+            stream_select($read, $null, $null, $timeout == null ? 1.0 : ($timeout / 3));
+
+            if (microtime(true) - $start > $timeout) {
+                return '';
+            }
+        }
+    }
+
+    public function readStderrLine($timeout = null)
+    {
+        return $this->_readLine($this->stderr, $timeout);
+    }
+
+    public function readLine($timeout = null)
+    {
+        return $this->_readLine($this->stdout, $timeout);
+    }
+
+    public function readLineIfAny()
+    {
+        if (!is_resource($this->stdout)) {
+            return '';
+        }
+        return fgets($this->stdout);
+    }
+
+    public function readStderrLineIfAny()
+    {
+        if (!is_resource($this->stderr)) {
+            return '';
+        }
+        return fgets($this->stderr);
+    }
+
+    public function close()
+    {
+        $this->returnCode = proc_close($this->process);
+        return $this->returnCode;
+    }
+
+    public function getReturnValue()
+    {
+        if (isset($this->returnCode)) {
+            return $this->returnCode;
+        }
+        $status = proc_get_status($this->process);
+        return $status['exitcode'];
+    }
+}
+
+//$r = new Runner('redis-cli');
+//
+//try {
+//    $r->send("GET test\n");
+//    print "Got line: " . $r->readLine() . "\n";
+//    $r->send("SET test 1\nGet test\n");
+//    print "Got line: " . $r->readLine() . "\n";
+//    print "Got line: " . $r->readLine() . "\n";
+//    print "Got line: " . $r->readLineIfAny() . "\n";
+//
+//    $r->send("fds\n");
+//    print "Got stderr line: " . $r->readStderrLineIfAny() . "\n";
+//    $code = $r->close();
+//} catch (Exception $e) {
+//}
+//
+//print "Got return value: " . $r->getReturnValue() . $r->readStderrLineIfAny() . "\n";
---
+++ src/RunnerInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+interface RunnerInterface
+{
+    public function isAlive();
+    public function send($string);
+    public function readStderrLine($timeout = null);
+    public function readLine($timeout = null);
+    public function readLineIfAny();
+    public function readStderrLineIfAny();
+    public function close();
+    public function getReturnValue();
+}
